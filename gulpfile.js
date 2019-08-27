a224f5b87f9fed09f2548cb7483be942abd319c2
Borys <Borys@DESKTOP-U977PG2>
2019-08-27 15:06:17 +0200

    prepared project for further work

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,36 @@
 const { series, src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
 
 sass.compiler = require('node-sass');
 
-function scss() {
+function scssDev() {
   return src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(concat())
     .pipe(dest('./css'));
 }
 
-function minifyCSS() {
-  return src('./style.css')
+function scssBuild() {
+  return src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
     .pipe(cleanCSS())
+    .pipe(concat('style.css'))
     .pipe(dest('./css'));
 }
 
 function scssWatch() {
-  return watch('./scss/**/*.scss', series(scss));
+  return watch('./scss/**/*.scss', series(scssDev));
 }
 
-exports.build = series(scss, minifyCSS);
+function jsDev() {
+
+}
+
+function jsBuild() {
+  
+}
+
+exports.build = series(scssBuild);
 exports.default = series(scssWatch);
